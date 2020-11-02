b0fadcfef0af37dbc8ccfac06ec6a155b4fd2111
Michael Guzman <mgzmn4@gmail.com>
2020-11-01 23:07:29 -0600

    Finished min requirements for hw3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes} from '@angular/router';
 import { TravelRequestComponent } from './travel-request/travel-request.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {path: 'travel-request', component: TravelRequestComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    TravelRequestComponent
+    TravelRequestComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     FormsModule
   ],
   providers: [],
